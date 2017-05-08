ff0bcc3301190ab836748008eb1b5351f9463791
Aaron Lesley <echoside@gmail.com>
2017-05-08 16:02:03 -0700

    Added comments

--- Sync.py
+++ Sync.py
@@ -4,15 +4,20 @@ import tkMessageBox
 from subprocess import call
 import pandas as pd
 
+# Builds the basic GUI
 top = Tkinter.Tk()
 top.resizable(width=False, height=False)
 top.minsize(width=300, height=300)
 top.title("SIS Google Sync")
 
-#def SIS():
+# def SIS():
+# Need SIS API
 
-#def GOOGLE():
+# def GOOGLE():
+# Need G-Suite API
 
+
+#merges the 2 CSV files by their Serial Number
 def compmerge():
     a = pd.read_csv("testid.csv")
     b = pd.read_csv("test.csv")
@@ -20,7 +25,7 @@ def compmerge():
     merged = a.merge(b, on='Serial Number')
     merged.to_csv("output.csv", index=False)
 
-
+# Buttons
 B1 = Tkinter.Button(top, text ="Pull From SIS", width =20)
 B2 = Tkinter.Button(top, text ="Pull From Google", width = 20)
 B3 = Tkinter.Button(top, text ="Sync CSV", width = 20, command = compmerge)
